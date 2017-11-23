e5f800ff87f98ee086601f5060abf08c75944665
cpphp <gzphper@163.com>
2017-11-23 11:37:09 +0800

    工厂模式v2版本代码

---
+++ 对象与设计/code/工厂模式/v2/BloggsApptEncoder.php
@@ -0,0 +1,10 @@
+<?php
+
+//blogg格式编码
+class BloggsApptEncoder extends ApptEncoder
+{
+	public function encode()
+	{
+		return 'appointement data encoded in blogg';
+	}
+}
---
+++ 对象与设计/code/工厂模式/v2/MegaApptEncoder.php
@@ -0,0 +1,10 @@
+<?php
+
+//mega格式编码
+class MegaApptEncoder extends ApptEncoder
+{
+	public function encode()
+	{
+		return 'appointement data encoded in mega';
+	}
+}
