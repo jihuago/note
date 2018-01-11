968af7b3f1b5248e7c54d2b184ef855bb4b8b8a4
cpphp <jihuagou@163.com>
2018-01-11 16:20:24 +0800

    匿名函数

---
+++ Anonymous_functions/anony_function_exampe1.php
@@ -0,0 +1,7 @@
+<?php
+
+echo preg_replace_callback('~-([a-z])~', function ($match) {
+
+	return strtoupper($match[1]);
+}, 'hello-world');
+
---
+++ Anonymous_functions/example4.php
@@ -0,0 +1,21 @@
+<?php
+
+/*class Button
+{
+	public static function click()
+	{
+		// 在php7+不能将$this use进去
+		$click = function () use ($this) {
+			$this->demo();
+		};
+	}
+
+	public function demo()
+	{
+		echo 123;
+	}
+}
+
+Button::click();*/
+
+  
