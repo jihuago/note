9e55f17ae955139e1c92ff755fd0502de7f9fa5f
linz <linz.3maio@gmail.com>
2021-04-12 05:45:27 +0000

    go 测试套件

--- go/src/demo/data_element/app/controllers/learnGoWithTest/integers/SumAll.go
+++ go/src/demo/data_element/app/controllers/learnGoWithTest/integers/SumAll.go
@@ -1,7 +1,11 @@
 package integers
 
-// 可变参数  ...类型
-func SumAll(numbersToSum ...[]int) []int {
+func DemoSumAll()  {
+	SumAll([]int{1, 2}, []int{5, 6})
+}
+
+/*// 可变参数  ...类型
+func SumAll(numbersToSum ...[]int) (sum []int) {
 	//return []int{3, 9}
 
 	lengthOfNumbers := len(numbersToSum)
@@ -11,6 +15,25 @@ func SumAll(numbersToSum ...[]int) []int {
 
 	for i, numbers := range numbersToSum {
 		sums[i] = Sum(numbers)
+	}
+
+	return
+}*/
+
+func SumAll(numbersToSum ...[]int) (sum []int) {
+
+	for _, numbers := range numbersToSum {
+		sum = append(sum, Sum(numbers))
+	}
+
+	return sum
+}
+
+func SumAllTails(numbersToSum ...[]int) []int  {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
 	}
 
 	return sums
