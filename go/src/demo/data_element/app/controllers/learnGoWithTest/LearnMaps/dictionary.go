655f6e90c7a9af8a094bfd3a22b4fca1aa97862d
linz <linz@qq.com>
2021-04-13 04:05:04 +0000

    map version2

--- go/src/demo/data_element/app/controllers/learnGoWithTest/LearnMaps/dictionary.go
+++ go/src/demo/data_element/app/controllers/learnGoWithTest/LearnMaps/dictionary.go
@@ -1,15 +1,20 @@
 package LearnMaps
 
-import "errors"
-
 // 创建了一个Dictionary类型，它是对map的简单封装。
 type Dictionary map[string]string
 
-var (
-	ErrNotFound = errors.New("could not find the word you were looking for")
-	ErrWordExists = errors.New("cannot add word because it already exists")
+const (
+	ErrNotFound = DictionaryErr("could not find the word you were looing for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
 )
 
+// 将错误声明为常量，可以让错误更具可重用性和不可变性
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string  {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error)  {
 
@@ -38,4 +43,23 @@ func (d Dictionary) Add(word, definition string) error {
 	}
 
 	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error  {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(word string)  {
+	delete(d, word)
 }
