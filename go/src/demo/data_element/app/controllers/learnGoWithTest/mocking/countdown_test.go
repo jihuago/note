1d4a776ee4f11212a841c125d20d11c7e4d0d7f1
linz <jihuagou@163.com>
2021-04-14 07:53:15 +0000

    mocking version2

--- go/src/demo/data_element/app/controllers/learnGoWithTest/mocking/countdown_test.go
+++ go/src/demo/data_element/app/controllers/learnGoWithTest/mocking/countdown_test.go
@@ -2,22 +2,50 @@ package mocking
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
 func TestCountdown(t *testing.T)  {
-	buffer := &bytes.Buffer{}
 
-	Countdown(buffer)
+	t.Run("sleep after every print", func(t *testing.T) {
+		spySleeper := &CountdownOperationsSpy{}
+		Countdown(spySleeper, spySleeper)
 
-	got := buffer.String()
-	want := `3
+		want := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleeper.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleeper.Calls)
+		}
+
+	})
+
+	t.Run("print 3 to Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+
+		// 更新测试以注入对我们监视器的依赖，并断言sleep被调用了4次
+		spySleeper := &SpySleeper{}
+
+		Countdown(buffer, spySleeper)
+
+		got := buffer.String()
+		want := `3
 2
 1
 Go!`
 
-	if got != want {
-		t.Errorf("got '%s' want '%s'", got, want)
-	}
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
+	})
 
 }
