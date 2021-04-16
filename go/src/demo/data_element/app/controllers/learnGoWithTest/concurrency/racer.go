2cf86a29cb02ea28578bd0b0fde0cd4284decbca
linz <linz@qq.com>
2021-04-16 03:16:31 +0000

    select version2

--- go/src/demo/data_element/app/controllers/learnGoWithTest/concurrency/racer.go
+++ go/src/demo/data_element/app/controllers/learnGoWithTest/concurrency/racer.go
@@ -23,7 +23,7 @@ func Racer(url1, url2 string) (winner string) {
 	return url2
 }*/
 
-// select 允许同时在多个channel等待。第一个发送值的channel胜出，case中的代码会被执行
+/*// select 允许同时在多个channel等待。第一个发送值的channel胜出，case中的代码会被执行
 func Racer(a, b string, timeout time.Duration) (winner string, err error) {
 	// 如果a或b谁先有结果，谁先返回，但如果测试达到10秒，那么time.After会发送一个信号并返回一个erro
 	select {
@@ -36,7 +36,24 @@ func Racer(a, b string, timeout time.Duration) (winner string, err error) {
 	case <-time.After(timeout):
 		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
+}*/
+var tenSecondTimeout = 10 * time.Second
+
+func Racer(a, b string) (winner string, err error)  {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
 }
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, err error)  {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waitting for %s and %s", a, b)
+	}
+}
+
 
 func ping(url string) chan bool {
 	ch := make(chan bool)
