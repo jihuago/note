1a18a8ee707fa9ffd86caf9ff636698a895c8010
linz <jihuagou@163.com>
2021-04-16 03:16:31 +0000

    select version2

--- go/src/demo/data_element/app/controllers/learnGoWithTest/concurrency/racer_test.go
+++ go/src/demo/data_element/app/controllers/learnGoWithTest/concurrency/racer_test.go
@@ -23,27 +23,44 @@ import (
 }*/
 
 func TestRacer(t *testing.T)  {
-	slowServer := httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-		time.Sleep(20 * time.Millisecond)
+	t.Run("compares speeds of servers, returning the url of the fastest one", func(t *testing.T) {
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
+
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
+
+		want := fastURL
+		got, err := Racer(slowURL, fastURL)
+
+		if err != nil {
+			t.Fatalf("did nt expect an error but got one %v", err)
+		}
+
+		if got != want {
+			t.Errorf("got '%s', want '%s'", got, want)
+		}
+
+		t.Run("return an error if a server doesn't respond within 10s", func(t *testing.T) {
+			server := makeDelayedServer(25 * time.Millisecond)
+
+			defer server.Close()
+
+			_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
+
+			if err == nil {
+				t.Errorf("expected an error but didn't get one")
+			}
+		})
+	})
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server  {
+	return httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		time.Sleep(delay)
 		writer.WriteHeader(http.StatusOK)
 	}))
-
-	defer slowServer.Close()
-
-	fastServer := httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-		writer.WriteHeader(http.StatusOK)
-	}))
-
-	defer fastServer.Close()
-
-	slowURL := slowServer.URL
-	fastUrl := fastServer.URL
-
-	want := fastUrl
-	got := Racer(slowURL, fastUrl)
-	if got != want {
-		t.Errorf("got '%s' want '%s'", got, want)
-	}
-
-
 }
