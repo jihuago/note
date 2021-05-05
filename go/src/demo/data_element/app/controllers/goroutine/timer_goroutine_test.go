26e991988e5cb0eb7060a87b912119ba9a3f80d4
linz <linz.3maio@gmail.com>
2021-05-05 06:52:18 +0000

    goroutine

--- go/src/demo/data_element/app/controllers/goroutine/timer_goroutine_test.go
+++ go/src/demo/data_element/app/controllers/goroutine/timer_goroutine_test.go
@@ -7,7 +7,11 @@ func TestRunTimer(t *testing.T) {
 		RunTimer()
 	})*/
 	
-	t.Run("test ticker update", func(t *testing.T) {
+/*	t.Run("test ticker update", func(t *testing.T) {
 		LimitRequest()
+	})
+*/
+	t.Run("test timer_goroutine_test", func(t *testing.T) {
+		DemoTimer()
 	})
 }
