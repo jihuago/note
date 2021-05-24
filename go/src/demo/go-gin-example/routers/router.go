e90e19b5df0c91f81d2ca526ffea773aafaf617a
vagrant <vagrant@homestead>
2021-05-24 17:30:21 +0800

    go-gin-demo

--- go/src/demo/go-gin-example/routers/router.go
+++ go/src/demo/go-gin-example/routers/router.go
@@ -3,6 +3,7 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/tmaio/go-gin-example/pkg/setting"
+	v1 "github.com/tmaio/go-gin-example/routers/api/v1"
 )
 
 func InitRouter() *gin.Engine {
@@ -11,11 +12,15 @@ func InitRouter() *gin.Engine {
 	r.Use(gin.Recovery())
 	gin.SetMode(setting.RunMode)
 
-	r.GET("/test", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "this is new test",
-		})
-	})
+	// 路由组
+	apiv1 := r.Group("/api/v1")
+	{
+		apiv1.GET("/tags", v1.GetTags)
+		apiv1.POST("/tags", v1.AddTag)
+		apiv1.PUT("/tags/:id", v1.EditTag)
+		apiv1.DELETE("/tags/:id", v1.DeleteTag)
+	}
+
 
 	return r
 }
