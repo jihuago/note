5259062f37326460c486acaa41caef37a3ac17a5
username <useremail@qq.com>
2021-05-26 17:48:17 +0800

    go-gin-demo bug

--- go/src/demo/go-gin-example/go.mod
+++ go/src/demo/go-gin-example/go.mod
@@ -5,6 +5,7 @@ go 1.15
 require (
 	github.com/OwnLocal/goes v1.0.0 // indirect
 	github.com/astaxie/beego v1.12.3 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gin-gonic/gin v1.7.1
 	github.com/go-ini/ini v1.62.0 // indirect
 	github.com/go-playground/validator/v10 v10.6.1 // indirect
---
+++ go/src/demo/go-gin-example/models/article.go
@@ -0,0 +1,86 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+	"time"
+)
+
+type Article struct {
+	Model
+	TagId int `json:"tag_id" gorm:"index"` // gorm:index 声明这个字段为索引，如果使用自动迁移功能则有影响，不使用无影响
+	Tag Tag `json:"tag"` // Tag 字段实际是一个嵌套的struct，利用TagId与Tag模型相互关联
+	Title string `json:"title"`
+	Desc string `json:"desc"`
+	Content string `json:"content"`
+	CreatedBy string `json:"created_by"`
+	ModifiedBy string `json:"modified_by"`
+	State int `json:"state"`
+}
+
+func (article *Article) BeforeCreate(scope *gorm.Scope) error {
+	scope.SetColumn("CreatedOn", time.Now().Unix())
+
+	return nil
+}
+
+func (article *Article) BeforeUpdate(scope *gorm.Scope) error {
+	scope.SetColumn("ModifiedOn", time.Now().Unix())
+
+	return nil
+}
+
+func ExistArticleById(id int) bool {
+	var article Article
+	db.Select("id").Where("id = ?", id).First(&article)
+
+	if article.ID > 0 {
+		return true
+	}
+
+	return false
+}
+
+func GetArticleTotal(maps interface{}) (count int) {
+	//db.Model(&Article{}).Where(maps).Count(&count)
+	db.Model(&Article{}).Where(maps).Count(&count)
+
+	return
+}
+
+func GetArticles(pageNum int, pageSize int, maps interface{}) (articles []Article) {
+	db.Preload("Tag").Where(maps).Offset(pageNum).Limit(pageSize).Find(&articles)
+
+	return
+}
+
+func GetArticle(id int) (article Article) {
+	db.Where("id = ?", id).First(&article)
+	db.Model(&article).Related(&article.Tag)
+
+	return
+}
+
+func EditArticle(id int, data interface{}) bool {
+	db.Model(&Article{}).Where("id = ?", id).Update(data)
+
+	return true
+}
+
+func AddArticle(data map[string]interface{}) bool {
+	db.Create(&Article{
+		TagId: data["tag_id"].(int),
+		Title: data["title"].(string),
+		Desc: data["desc"].(string),
+		Content: data["content"].(string),
+		CreatedBy : data["created_by"].(string),
+		State : data["state"].(int),
+	})
+
+	return true
+}
+
+func DeleteArticle(id int) bool {
+	db.Where("id = ?", id).Delete(&Article{})
+
+	return true
+}
