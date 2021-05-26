d7ee8a6808ff7c73afd0a3e2fca5bbbc80bd2321
username <jihuagou@163.com>
2021-05-26 17:48:17 +0800

    go-gin-demo bug

---
+++ go/src/demo/go-gin-example/pkg/util/jwt.go
@@ -0,0 +1,49 @@
+package util
+
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/tmaio/go-gin-example/pkg/setting"
+	"time"
+)
+
+var jwtSecret = []byte(setting.JwtSecret)
+
+type Claims struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	jwt.StandardClaims
+}
+
+func GeneratoToken(username, password string) (string, error) {
+	nowTime := time.Now()
+	expireTime := nowTime.Add(3 * time.Hour)
+
+	claims := Claims{
+		username,
+		password,
+		jwt.StandardClaims{
+			ExpiresAt: expireTime.Unix(),
+			Issuer: "gin-blog",
+		},
+	}
+
+	tokenClaims := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+	token, err := tokenClaims.SignedString(jwtSecret)
+	fmt.Println(err)
+	return token, err
+}
+
+func ParseToken(token string) (*Claims, error) {
+	tokenClaims, err := jwt.ParseWithClaims(token, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return jwtSecret, nil
+	})
+
+	if tokenClaims != nil {
+		if claims, ok := tokenClaims.Claims.(*Claims); ok && tokenClaims.Valid {
+			return claims, nil
+		}
+	}
+
+	return nil, err
+}
